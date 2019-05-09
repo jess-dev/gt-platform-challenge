6d39676841aa962a43d0f6f8b93d6bfc7831a825
Jessica Taylor <jtaylor@simplypropertylawyers.co.uk>
2019-05-09 21:03:50 +0100

    Added GUI to start the input

--- PlatformChallenge/PlatformChallenge/Program.cs
+++ PlatformChallenge/PlatformChallenge/Program.cs
@@ -12,9 +12,21 @@ namespace PlatformChallenge
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("            ╒════════════╕                 ");
+            Console.WriteLine("            │  Platform  │                 ");
+            Console.WriteLine("            │    Team    │                 ");
+            Console.WriteLine("            │  Challenge │                 ");
+            Console.WriteLine("            ╘════════════╛                 ");
+            Console.WriteLine("                                           ");
+            Console.WriteLine("Please press return to start the CSV input.");
+            Console.ReadLine();
+
+            // Parse CSV
             var input = CSVInput.Parse(appSettings.File);
+
             if (input != null)
             {
+                // Post each record to Webhook site every 5 seconds
                 foreach (ParsedRecord record in input)
                 {
                     WebHookInterface.Post(record);
--- PlatformChallenge/PlatformChallenge/appSettings.cs
+++ PlatformChallenge/PlatformChallenge/appSettings.cs
@@ -9,6 +9,6 @@ namespace PlatformChallenge
     class appSettings
     {
         public static string WebhookPath = "https://webhook.site/ec8d6542-74ff-4fce-a0ed-0e6a4aa1b464";
-        public static string File = @"C:\Users\jtaylor\source\repos\gt-challenge\testfile.csv";
+        public static string File = @"C:\Users\jtaylor\source\repos\gt-challenge\squeaks.csv";
     }
 }
